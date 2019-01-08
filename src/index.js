d49153b8f94dd8d1c585efdd1717e15d4235d47b
makoto-hiramatsu <makoto-hiramatsu@cookpad.com>
2019-01-08 21:15:19 +0900

    Show more information about a paper

--- src/index.js
+++ src/index.js
@@ -3,17 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3TV9KzYftbwV0gtWRZpTjMz9PSGHDU25705dbazQ_jmCYasQeL1YKP3jfZFY2kZ7PMWCYAVjLVG8h/pub?gid=0&single=true&output=tsv'
+const URL = 'https://docs.google.com/spreadsheets/d/e/' +
+  '2PACX-1vQ3TV9KzYftbwV0gtWRZpTjMz9PSGHDU25705dbazQ_jmCYasQeL1YKP3jfZFY2kZ7PMWCYAVjLVG8h/' +
+  'pub?gid=0&single=true&output=tsv'
+
+
+function toHTML(paper_json, index) {
+  let list = []
+  list.push(<li> added: {paper_json['date'] } </li>)
+  list.push(<li> in {paper_json['conference'] || 'Unknown'} {paper_json['year']} </li>)
+
+  if (paper_json.note) {
+    list.push(<li> {paper_json['note']} </li>)
+  }
+
+  return (
+    <ul key={index}>
+      <li><a href={paper_json['link']}>{paper_json['title']}</a></li>
+      <ul>{list}</ul>
+    </ul>
+  )
+}
 
 
 function toJSON(records) {
   records = records.map(function(record) {
-    return record.split('\t')
-  })
+    return record.split('\t')})
 
   var columns = records[0]
   var papers = records.slice(1,)
-
   var papers_json = papers.map(function(paper) {
     var dict = {}
     for (var i=0; i<columns.length; i++) dict[columns[i]] = paper[i]
@@ -31,17 +49,8 @@ function parseTSV(response) {
   console.log(papers_json);
   return (
     <ul>
-      {papers_json.map(function(paper, index){
-        return (
-          <ul key={index}>
-            <li><a href={paper['link']}>{paper['title']}</a></li>
-            <ul>
-              <li> added: {paper['date'] } </li>
-              <li> {paper['note']} </li>
-            </ul>
-          </ul>
-        )
-      })}
+      {papers_json.map(
+        (paper_json, index) => toHTML(paper_json, index))}
     </ul>
   )
 }
