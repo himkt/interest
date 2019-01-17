088896851fee400561c82805ff6a06428d4aeeff
makoto-hiramatsu <makoto-hiramatsu@cookpad.com>
2019-01-17 18:03:02 +0900

    Support to export BibTeX item of a journal article

--- src/components/molecules/card.jsx
+++ src/components/molecules/card.jsx
@@ -1,41 +1,84 @@
 import React from 'react'
 import PaperBadge from '../atoms/paperBadge'
 import Popup from 'reactjs-popup'
+import { endianness } from 'os';
 
 type Props = {
     paper: any,
     idx: number
 }
 
+
+function extractVolumeNumber(paper_json) {
+    let year_volume_number = paper_json['year'].split(',')
+    let arr_size = year_volume_number.length
+
+    if (arr_size == 2) {
+        return [year_volume_number[1], '-1']
+    }
+    else if (arr_size == 3) {
+        return [year_volume_number[1], year_volume_number[2]]
+    }
+    else {
+        return ['-1', '-1']
+    }
+}
+
+
+function normalizeYear(paper_json) {
+    return paper_json['year'].split(',')[0]
+}
+
+
+function normalizeBookName(paper_json) {
+    let book_name = paper_json['conference']
+    if (book_name.startsWith('j:')) {
+        return book_name.slice(2,)
+    }
+    else {
+        return book_name
+    }
+}
+
+
 function createKey(paper_json) {
     let raw_authors = paper_json['authors'].split(',')
+    let year = normalizeYear(paper_json)
     if (raw_authors.length > 0) {
         let first_author = raw_authors[0]
         first_author = first_author.split(' ')
         first_author = first_author.slice(-1)[0]
-        return first_author + paper_json['year']
+        return first_author + year
     }
 }
 
 function createPopup(paper_json) {
     let bibtex_item = ''
+    let book_name = normalizeBookName(paper_json)
 
     if (paper_json['conference'].startsWith('j:')) {
-        let journal_name = paper_json['conference'].slice(2)
         bibtex_item += '@article{' + createKey(paper_json) + ',<br>'
-        bibtex_item += '&nbsp;&nbsp;journal={' + journal_name + '},<br>'
+        bibtex_item += '&nbsp;&nbsp;journal={' + book_name + '},<br>'
+
+        let [volume, number] = extractVolumeNumber(paper_json)
+        
+        if (volume != '-1') {
+            bibtex_item += '&nbsp;&nbsp;volume={' + volume + '},<br>'
+        }
+        if (number != '-1') {
+            bibtex_item += '&nbsp;&nbsp;number={' + number + '},<br>'
+        }
+
     } else {
-        createKey(paper_json)
         bibtex_item += '@inproceedings{' + createKey(paper_json) + ',<br>'
-        bibtex_item +=
-            '&nbsp;&nbsp;booktitle={Proceedings of ' +
-            paper_json['conference'] +
-            '},<br>'
+        bibtex_item += '&nbsp;&nbsp;booktitle={Proceedings of ' +
+            book_name + '},<br>'
     }
 
     let authors = paper_json['authors'].replace(/, /g, ' and ')
     bibtex_item += '&nbsp;&nbsp;author="' + authors + '",<br>'
     bibtex_item += '&nbsp;&nbsp;title={' + paper_json['title'] + '},<br>'
+    bibtex_item += '&nbsp;&nbsp;year={' + normalizeYear(paper_json) + '},<br>'
     bibtex_item += '}'
 
     let ret = { __html: bibtex_item }
@@ -60,11 +103,11 @@ const Card = ({ paper, idx }: Props) => (
             <div className="headerComponent">
                 {createPopup(paper)}
                 <PaperBadge
-                    text={paper.conference}
+                    text={normalizeBookName(paper)}
                     badgeClass="badge badge-info"
                 />
                 <PaperBadge
-                    text={paper.year}
+                    text={normalizeYear(paper)}
                     badgeClass="badge badge-success"
                 />
             </div>
