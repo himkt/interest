0df5cba3958a450aadc83ec3e9f3eade1d529682
Makoto Hiramatsu <himkt@cookpad.com>
2021-05-05 21:18:55 +0900

    Modify progress bar

--- src/atoms/loading.tsx
+++ src/atoms/loading.tsx
@@ -4,9 +4,13 @@ import React from 'react'
 const LoadingContainer = () => {
 
   return (
-    <section style={{ padding: '3rem' }}>
-      <div className="container is-max-widescreen">
-        <progress className="progress is-medium is-dark" max="100">Loading...</progress>
+    <section className="section">
+      <div className="columns is-mobile is-centered">
+        <div className="column is-half">
+          <progress className="progress is-large is-danger">Loading...</progress>
+          <progress className="progress is-large is-success">Loading...</progress>
+          <progress className="progress is-large is-info">Loading...</progress>
+        </div>
       </div>
     </section>
   )
